40cde8ab87654e4f57acad5aa2cc31b16a7e3c35
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-13 16:32:51 +0530

    Add chapter 5

---
+++ Chapter 5/02_dict_methods.py
@@ -0,0 +1,15 @@
+marks = {
+    "Rafikul": 100,
+    "Shubham": 56,
+    "Rohan": 23,
+    0: "Rafikul"
+}
+
+# print(marks.items())
+# print(marks.keys())
+# print(marks.values())
+# marks.update({"Rafikul": 99, "Renuka": 100})
+# print(marks)
+
+print(marks.get("Rafikul2")) # Prints None
+print(marks["Rafikul2"]) # Returns an error
