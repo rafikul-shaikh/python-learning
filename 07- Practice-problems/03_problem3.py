15f357563bc5d23006e325e250fbe33d89e3e7b7
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-14 11:20:09 +0530

    Add o0-module practice problems

---
+++ 07- Practice-problems/03_problem3.py
@@ -0,0 +1,10 @@
+#Write a program to print multiplication table of a given number using while loop.
+
+n = int(input("Enter a number: "))
+
+i = 1
+
+while(i<11):
+    print(f"{n} X {i} = {n * i}")
+    i += 12
+    
