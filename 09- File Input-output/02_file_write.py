fca0dc3e80960c6f9c3752754adb0f7cd707bec3
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-14 18:41:42 +0530

    Add 09 - module , File input/output

---
+++ 09- File Input-output/02_file_write.py
@@ -0,0 +1,7 @@
+st = "Hey Rafikul you are amazing"
+
+f = open("myfile.txt", "w")
+
+f.write(st)
+
+f.close()
---
+++ 09- File Input-output/04_append.py
@@ -0,0 +1,7 @@
+st = "Hey Rafikul you are amazing"
+
+f = open("myfile.txt", "a")
+
+f.write(st)
+
+f.close()
