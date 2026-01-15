b057d5e861fc5f1ae441553d03a773b9dd5cddfd
Rafikul shaikh <rafikulshaikh510@gmail.com>
2026-01-15 11:17:50 +0530

    Add 10-module , OOPs in python

---
+++ 10- OOPs in python/02_instance_vs_class_attr.py
@@ -0,0 +1,9 @@
+class Employee: 
+    language = "Python" # This is a class attribute
+    salary = 1200000
+
+
+rafikul = Employee()
+rafikul.language = "JavaScript" # This is an instance attribute
+print(rafikul.language, rafikul.salary)
+ 
